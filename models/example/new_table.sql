88b1f3c38a642a05fe0d2100d1e1e52866180757
Naveen Patnana <naveenpatnana@MapleMonks-MacBook-Air.local>
2021-11-18 13:14:25 +0530

    newTable

--- models/example/new_table.sql
+++ models/example/new_table.sql
@@ -6,7 +6,7 @@
                 }
             ) }}
             with sample_data as (
-                select * from 'DEMO_DB.undefined.new_table'
+                select * from 'DEMO_DB.MAYANK.new_table'
             ),
             
             final as (
