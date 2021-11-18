96d5f87a2675072b761df503b757881aa3e81dde
Naveen Patnana <naveenpatnana@MapleMonks-MacBook-Air.local>
2021-11-18 12:20:38 +0530

    model file has updated

---
+++ models/example/new_table.sql
@@ -0,0 +1,16 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "UPDATE new_table SET number=444 where number=1;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+                select * from 'DEMO_DB.undefined.new_table'
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
