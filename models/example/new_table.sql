21517a76ab21afbc21acee6732cfb60c7da4ff1a
Naveen Patnana <naveenpatnana@MapleMonks-MacBook-Air.local>
2021-11-18 17:23:12 +0530

    model file has updated

---
+++ models/example/new_table.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "UPDATE new_table SET number=444 where number=1;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from DEMO_DB.mayank.new_table
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
