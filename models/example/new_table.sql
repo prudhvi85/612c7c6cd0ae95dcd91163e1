d3ad798d7ae76ffdc893afdb5d42c32538a42d78
root <root@ip-172-31-46-240.ap-south-1.compute.internal>
2021-11-19 10:26:50 +0000

    model file has updated

---
+++ models/example/new_table.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "UPDATE DEMO_DB.mayank.new_table SET number = 1001 where number=36;, ALTER TABLE DEMO_DB.mayank.new_table ADD newCol varchar(255);, SELECT *, (id + number) AS Sum FROM DEMO_DB.mayank.new_table;",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from DEMO_DB.mayank.new_table
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
