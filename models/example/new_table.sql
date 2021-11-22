799879663089ffb2310c6f09a36ecf946b798ca2
Naveen Patnana <naveenpatnana@MapleMonks-MacBook-Air.local>
2021-11-22 22:46:43 +0530

    create statemnet

--- models/example/new_table.sql
+++ models/example/new_table.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "alter table DEMO_DB.AKA_DB.NAVEEN_Test RENAME TO DEMO_DB.AKA_DB.NAV_HOPE_TEST;",
+                    "sql": "Create table DEMO_DB.AKA_DB.EXPERIMENT(name varchar(30),deci float);;",
                     "transaction": true
                 }
             ) }}
