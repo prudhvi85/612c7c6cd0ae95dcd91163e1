d4e60a28d96171d333132d4420c811b76b6a92f2
Naveen Patnana <naveenpatnana@MapleMonks-MacBook-Air.local>
2021-11-23 16:05:03 +0530

    testing phase

--- models/example/databases.sql
+++ models/example/databases.sql
@@ -1,17 +1,8 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "alter table DEMO_DB.AKA_DB.NAV_HOPE_TEST RENAME TO DEMO_DB.AKA_DB.NAVEEN_Test;",
+                    "sql": "alter table DEMO_DB.AKA_DB.NAVEEN_TEST RENAME TO DEMO_DB.AKA_DB.NAV_HOPE_TEST;",
                     "transaction": true
                 }
             ) }}
-            with sample_data as (
-
-                select * from DEMO_DB.information_schema.databases
-            ),
-            
-            final as (
-                select * from sample_data
-            )
-            select * from final
             
