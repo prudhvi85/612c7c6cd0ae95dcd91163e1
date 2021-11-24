9dd3e242ce310242184b39914a607c711d4f550b
root <root@ip-172-31-46-240.ap-south-1.compute.internal>
2021-11-24 14:31:39 +0000

    model file has updated

---
+++ models/example/databases.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "UPDATE summary_data SET current_category = (SELECT category_id FROM products WHERE products.product_id = summary_data.product_id) WHERE EXISTS (SELECT category_id FROM products WHERE products.product_id = summary_data.product_id);",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from DEMO_DB.information_schema.databases
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
