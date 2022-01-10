a005dd33ca88c08d94bc9f1b3a0eb4505bc5bcf7
naveenpatnana <90193804+naveenpatnana@users.noreply.github.com>
2022-01-10 18:24:30 +0530

    Update Ecommerce.sql

--- models/example/Ecommerce.sql
+++ models/example/Ecommerce.sql
@@ -1,17 +1,1 @@
-{{ config(
-            materialized='table',
-                post_hook={
-                    "sql": "DROP TABLE IF EXISTS NEWTABLE; CREATE TABLE NEWTABLE (PARTNO SMALLINT NOT NULL, DESCR VARCHAR(24), QONHAND INT, PRIMARY KEY(PARTNO));",
-                    "transaction": true
-                }
-            ) }}
-            with sample_data as (
-
-                select * from DEMO_DB.NoNorm.nonlocations
-            ),
-            
-            final as (
-                select * from sample_data
-            )
-            select * from final
-            
+select * from DEMO_DB.NONORM._AIRBYTE_RAW_NONLOCATIONS;
