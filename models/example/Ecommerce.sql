f97a7cb2743fcdfc81a4f915624ea5ed8b3c55ff
naveenpatnana <naveen_patnana@maplemonk.com>
2022-01-10 11:47:48 +0000

    model file has updated

---
+++ models/example/Ecommerce.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "DROP TABLE IF EXISTS NEWTABLE; CREATE TABLE NEWTABLE (PARTNO SMALLINT NOT NULL, DESCR VARCHAR(24), QONHAND INT, PRIMARY KEY(PARTNO));",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from DEMO_DB.maplemonk.Ecommerce
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
