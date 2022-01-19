869ddb7747689f29f8919933217479893941a620
naveenpatnana <naveen_patnana@maplemonk.com>
2022-01-19 13:57:35 +0530

    testing only sql

--- models/example/Ecommerce.sql
+++ models/example/Ecommerce.sql
@@ -1,1 +1,7 @@
-select _AIRBYTE_DATA from DEMO_DB.NONORM._AIRBYTE_RAW_NONLOCATIONS
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "CREATE TABLE DEMO_DB.MAPLEMONK.Persons (PersonID int,LastName varchar(255));",
+                    "transaction": true
+                }
+            ) }}
