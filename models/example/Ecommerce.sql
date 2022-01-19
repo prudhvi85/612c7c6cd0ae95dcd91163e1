c0b524a154e56efecee5802036d27c0a925e97b4
naveenpatnana <naveen_patnana@maplemonk.com>
2022-01-19 14:03:34 +0530

    testing only sql

--- models/example/Ecommerce.sql
+++ models/example/Ecommerce.sql
@@ -1,7 +1,7 @@
 {{ config(
             materialized='table',
                 post_hook={
-                    "sql": "CREATE TABLE DEMO_DB.MAPLEMONK.Persons (PersonID int,LastName varchar(255));",
+                    "sql": "CREATE TABLE DEMO_DB.MAPLEMONK.Persons(PersonID int,LastName varchar(255));",
                     "transaction": true
                 }
             ) }}
