1cfae8e7d5dd48c0c438f2e4e790f1d2f69248e4
root <root@ip-172-31-46-240.ap-south-1.compute.internal>
2022-01-21 14:18:14 +0000

    model file has updated

--- models/example/sqltest_BigEcom.sql
+++ models/example/sqltest_BigEcom.sql
@@ -2,7 +2,7 @@
             materialized='table',
                 post_hook={
                     "sql": "drop table if exists demo_db.batchtest.newPersona;
-create table demo_db.batchtest.newPersona(storm varchar(25)); ;",
+create table demo_db.batchtest.newPersona(storm varchar(25));",
                     "transaction": true
                 }
             ) }}
