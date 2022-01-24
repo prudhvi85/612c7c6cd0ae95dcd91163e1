1b8775dbe753612707eafbe8cf4c18736f53d17c
root <root@ip-172-31-46-240.ap-south-1.compute.internal>
2022-01-24 10:04:11 +0000

    model file has updated

---
+++ models/example/toRem_Ecommerce.sql
@@ -0,0 +1,17 @@
+{{ config(
+            materialized='table',
+                post_hook={
+                    "sql": "create table demo_db.batchtest.newP(storm varchar(25));",
+                    "transaction": true
+                }
+            ) }}
+            with sample_data as (
+
+                select * from DEMO_DB.batchtest.toRem_Ecommerce
+            ),
+            
+            final as (
+                select * from sample_data
+            )
+            select * from final
+            
