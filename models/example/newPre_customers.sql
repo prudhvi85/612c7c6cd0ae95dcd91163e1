a8f93c465b932beadc948171c28bf54a31e9c8b1
naveenpatnana <naveen_patnana@maplemonk.com>
2022-01-28 13:57:50 +0530

    model file has updated

--- models/example/newPre_customers.sql
+++ models/example/newPre_customers.sql
@@ -1,18 +1,18 @@
 {{ config(
-            materialized='table',
-                post_hook={
-                    "sql": "drop table if exists demo_db.batchtest.newPersona;
+                        materialized='table',
+                            post_hook={
+                                "sql": "drop table if exists demo_db.batchtest.newPersona;
 create table demo_db.batchtest.newPersona(storm varchar(25));",
-                    "transaction": true
-                }
-            ) }}
-            with sample_data as (
+                                "transaction": true
+                            }
+                        ) }}
+                        with sample_data as (
 
-                select * from DEMO_DB.batchtest.newPre_customers
-            ),
-            
-            final as (
-                select * from sample_data
-            )
-            select * from final
-            
+                            select * from DEMO_DB.batchtest.newPre_customers
+                        ),
+                        
+                        final as (
+                            select * from sample_data
+                        )
+                        select * from final
+                        
