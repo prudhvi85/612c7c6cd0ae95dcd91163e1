4e6a58d4fa078c1b478ed4e784ebcb44ac2aa7d1
aakashkumarmaple <aakashkumarmaple@maplemonk.com>
2022-02-08 11:16:27 +0530

    final change with proper cursor feilds

--- models/example/deduping_generic_script.sql
+++ models/example/deduping_generic_script.sql
@@ -16,17 +16,20 @@ input_data as (
 
    select {{ var('rows') }}
 --    from "DEMO_DB"."DESTSET"."DELETE2SPONSORED_DISPLAY_REPORT_STREAM"
-   from "DEMO_DB"."DESTSET".{{var('table')}}
+   from {{var('table')}}
 -- dbt run --vars='{"prefix": "test_", "rows": "Profileid, Metric,  Metric:adGroupId as adGroupId"}'   
 ),
  new_updated as (
    SELECT * FROM (
         select *, row_number() over(
             partition by {{var("partitionRows")}}
-            order by Reportdate desc
-        ) AS {{var("cursor_feild")}}
+            order by 
+            {{var("cursor_feild")}} is null asc,
+            {{var("cursor_feild")}} desc,
+            _AIRBYTE_EMITTED_AT desc
+        ) AS ROW_NUMBER
       FROM input_data
-     ) WHERE {{var("cursor_feild")}} = 1
+     ) WHERE ROW_NUMBER = 1
 )
 SELECT {{var("orignalField")}}  FROM new_updated
 
